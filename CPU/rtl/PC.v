1302e3994092769f81bd630741ab3fb7ef049fde
Romantichips <dramaticookies@qq.com>
2026-08-21 18:51:05 +0800

    1

--- CPU/rtl/PC.v
+++ CPU/rtl/PC.v
@@ -1,59 +1,20 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2023/06/30 14:19:39
-// Design Name: 
-// Module Name: PC
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module pc(
+    input             clk           ,
+    input             rst_n         ,
+    input             pc_hold       ,
+    input      [31:0] inst_addr_i   ,
+    output reg [31:0] inst_addr_o
+);
 
-module PC(
-    input wire clk,
-    input wire rst,
-    input wire stall,
-    input wire [31:0] din,
-    output reg [31:0] pc
-    );
-
-
-always @(posedge clk or posedge rst) begin
-    if(rst==`RstEnable) begin
-        pc <= 32'd0;
-    end else if(stall) begin
-        pc <= pc;
-    end else begin
-        pc <= din;
-    end
+always @(posedge clk) begin
+    if(!rst_n)
+        inst_addr_o <= 32'h8000_0000; //复位也可以是32'd0，为了波形上好区分，这是�?-32'd4
+	//inst_addr_o <= 32'd0;
+    else if(pc_hold == 1'b1)
+            inst_addr_o <= inst_addr_o; //PC暂停
+         else 
+            inst_addr_o <= inst_addr_i;
 end
-
-//reg flag;
-//always @(posedge clk or posedge rst) begin
-//    if(rst==`RstEnable) begin
-//        pc <= 32'd0;
-//        flag <= 1;
-//    end else if(flag==1) begin
-//        pc <= 0;
-//        flag <= 0;
-//    end else if(stall) begin
-//        pc <= pc;
-//        flag <= flag;
-//    end else begin
-//        pc <= din;
-//        flag <= flag;
-//    end
-//end
 
 endmodule
